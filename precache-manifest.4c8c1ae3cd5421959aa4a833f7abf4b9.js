3edb5433932a340e4766169c53baf3e3d0c6fa90
gokulcodes <gokul282@outlook.com>
2020-07-25 20:38:54 +0530

    Updates

---
+++ precache-manifest.4c8c1ae3cd5421959aa4a833f7abf4b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc62f8a32cdb321d6547f4124d7b005",
+    "url": "/gokulvaradan/index.html"
+  },
+  {
+    "revision": "25748d372ad2fe23b85a",
+    "url": "/gokulvaradan/static/css/main.dfa5e710.chunk.css"
+  },
+  {
+    "revision": "d1b3c5cd0a7868c3d58e",
+    "url": "/gokulvaradan/static/js/2.3fab5595.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gokulvaradan/static/js/2.3fab5595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25748d372ad2fe23b85a",
+    "url": "/gokulvaradan/static/js/main.32245a8d.chunk.js"
+  },
+  {
+    "revision": "1b39df1c5efb9e78c921",
+    "url": "/gokulvaradan/static/js/runtime-main.be6c11f1.js"
+  }
+]);
