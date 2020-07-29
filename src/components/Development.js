a9218d71109a2a3e4300a76b67fe0a7b26e8a762
gokulcodes <gokul282@outlook.com>
2020-07-29 19:36:43 +0530

    stage-8

--- src/components/Development.js
+++ src/components/Development.js
@@ -10,7 +10,7 @@ function Development() {
     className="flex flex-col justify-center items-center">
             <p className="text-lg md:text-xl w-4/5 text-center font-light">
             I'm a full stack developer and UI/UX specialist, very passionate about learning new things and building new excellent softwares around us. 
-            I've quite experience by working in Social Projects from individual client to small startups. Some of my works are listed below. Also visit: <a href="https://github.io/gokulcodes">gokulcodes</a> for more development works.
+            I've quite experience by working in Social Projects from individual client to small startups. Some of my works are listed below. Also visit: <a href="https://github.io/gokulcodes" className="text-yellow-600">gokulcodes</a> for more development works.
             </p>
             {/* fullstack */}
         <div class="w-9/12 px-5 py-16 mx-auto flex flex-wrap z-0 flex-col">
